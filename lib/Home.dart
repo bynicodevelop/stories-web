d0b973fcb427f75db3d2660b4ade016f72a04a12
nico-develop <contact@nico-develop.fr>
2021-02-20 17:14:26 +0100

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints(
+            minWidth: 300,
+            maxWidth: 600,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 0,
+                ),
+                child: Text(
+                  'Stories',
+                  style: Theme.of(context).textTheme.headline1,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 5.0,
+                ),
+                child: Text(
+                  'Stories est en cours développement.',
+                  style: Theme.of(context).textTheme.bodyText1,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 5.0,
+                ),
+                child: Text(
+                  "Mais bonne nouvelle, vous pouvez avoir un avant good en téléchargent l'application de lancement.",
+                  style: Theme.of(context).textTheme.bodyText1,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 5.0,
+                ),
+                child: Text(
+                  "Rejoignez-nous dans cette aventure en cliquant sur le bouton juste en dessous.",
+                  style: Theme.of(context).textTheme.bodyText1,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  height: 70,
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    child: Image.asset(
+                      'assets/images/play-store.png',
+                    ),
+                    onTap: () => launch('#'),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:web/Home.dart';
+import 'package:services/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(
+    Services(
+      child: App(),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Run new stories...',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: TextTheme(
+          headline1: GoogleFonts.nunitoSans(),
+          bodyText1: GoogleFonts.nunitoSans().copyWith(),
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
